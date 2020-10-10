b531d06f7644d22634ad5b56b1a836e6a7e13704
Giacomo Saracchi <6.saracchi@gmail.com>
2020-10-10 18:04:49 +0100

    Update event_setter.py

--- event_setter.py
+++ event_setter.py
@@ -6,22 +6,24 @@ Created on Sat Oct 10 11:16:47 2020
 @author: giacomosaracchi
 """
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from threading import Timer
 import calendar
 
 # event is function to execute. No () after name
 # time is string made of separated hours, minutes
 # day is int (up to the end of the month)
-
-# 
+# month is int (up to 12)
 
 class SetEvent:
     
-    def __init__(self, event, time, day=datetime.today().day):
+    today = datetime.today()
+    
+    def __init__(self, event, time, day=today.day, month=today.month):
         self.event = event
-        self.day = day
         self.time = time
+        self.day = day
+        self.month = month
         self.today = datetime.today()
         self.split_time()
         self.set_timer()
@@ -37,9 +39,8 @@ class SetEvent:
                     
     def set_timer(self):
         today = self.today
-        set_time = today.replace(day=self.day, hour=self.hour, minute=self.minute,
-                                 second=self.second) 
-        set_time += timedelta(days=self.day - today.day)
+        set_time = today.replace(month=self.month, day=self.day, hour=self.hour, 
+                                 minute=self.minute, second=self.second)
         delta_t = set_time - today
         secs_to_event = delta_t.total_seconds() 
         self.timer = Timer(secs_to_event, self.event)
@@ -57,10 +58,15 @@ class SetEvent:
         today = datetime.today()
         current_month_length = calendar.monthrange(today.year, today.month)[1]
         if value > current_month_length:
-            raise ValueError("Can't set for after the end of the month")
+            raise ValueError(f"Up to {current_month_length} days in {calendar.month_name[today.month]}")
         self._day = value
         
-        
-        
-        
-        
+    @property
+    def month(self):
+        return self._month
+    
+    @month.setter
+    def month(self, value):
+        if value > 12:
+            raise ValueError("Up to 12 months in a year")
+        self._month = value
